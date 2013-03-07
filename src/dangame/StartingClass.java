f4b73102572841860f2038d736fef16e9eef90d6
Dan Ahern <danahern@gmail.com>
2013-03-06 23:57:03 -0500

    updating game

--- src/dangame/StartingClass.java
+++ src/dangame/StartingClass.java
@@ -5,19 +5,24 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
+	private Robot robot;
+
 	@Override
 	public void init() {
 		setSize(800, 600);
 		setBackground(Color.BLACK);
 		setFocusable(true);
+		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Q-bot Alpha");
 	}
 
 	@Override
 	public void start() {
+		robot = new Robot();
 		Thread thread = new Thread(this);
 		thread.start();
 	}
@@ -43,22 +48,46 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 			}
 		}
 	}
+	
+	
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			break;
+		case KeyEvent.VK_S:
+			break;
+		case KeyEvent.VK_A:
+			break;
+		case KeyEvent.VK_D:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			break;
+		case KeyEvent.VK_S:
+			break;
+		case KeyEvent.VK_A:
+			break;
+		case KeyEvent.VK_D:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
