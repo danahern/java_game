7c1fc24162a9646f215efedc2ebd670dc33f94cd
Dan Ahern <danahern@gmail.com>
2013-03-07 11:14:48 -0500

    Changes to get robot to show

--- src/dangame/Robot.java
+++ src/dangame/Robot.java
@@ -12,29 +12,30 @@ public class Robot {
 
 		// Moves Character or Scrolls Background accordingly.
 		if (speedX < 0) {
-			centerX += speedX;
-		} else if (speedX == 0) {
-			System.out.println("Do not scroll the background.");
+			centerX += speedX; //This changes centerX by adding speedX.
+		} else if (speedX == 0) { 
+			// System.out.println("Do not scroll the background.");
 
 		} else {
-			if (centerX <= 150) {
-				centerX += speedX;
-			} else {
-				System.out.println("Scroll Background Here");
+			if (centerX <= 150) { //If the character's centerX is in the left 150 pixels
+				centerX += speedX; //Change centerX by adding speedX.
+			} else { 
+				System.out.println("Scroll Background Here"); //
 			}
 		}
-
+		
 		// Updates Y Position
-
 		if (centerY + speedY >= 382) {
+              //382 is where the character's centerY would be if he were standing on the ground.
 			centerY = 382;
-		} else {
-			centerY += speedY;
-		}
+		}else{                      
+                     centerY += speedY; //Add speedY to centerY to determine its new position
+                }
 
 		// Handles Jumping
 		if (jumped == true) {
-			speedY += 1;
+			speedY += 1; //While the character is in the air, add 1 to his speedY.     
+                       //NOTE: This will bring the character downwards!
 
 			if (centerY + speedY >= 382) {
 				centerY = 382;
@@ -45,11 +46,52 @@ public class Robot {
 		}
 
 		// Prevents going beyond X coordinate of 0
-		if (centerX + speedX <= 60) {
+		if (centerX + speedX <= 60) { //If speedX plus centerX would bring the character     //outside the screen,
 			centerX = 61;
+                       //Fix the character's centerX at 60 pixels.
 		}
 	}
+	
+	public int getCenterX() {
+		return centerX;
+	}
 
+	public int getCenterY() {
+		return centerY;
+	}
+
+	public boolean isJumped() {
+		return jumped;
+	}
+
+	public int getSpeedX() {
+		return speedX;
+	}
+
+	public int getSpeedY() {
+		return speedY;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+
+	public void setJumped(boolean jumped) {
+		this.jumped = jumped;
+	}
+
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+	
 	public void moveRight() {
 		speedX = 6;
 	}
--- src/dangame/StartingClass.java
+++ src/dangame/StartingClass.java
@@ -3,21 +3,34 @@ package dangame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.Graphics;
+import java.net.URL;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
 	private Robot robot;
+	private Image image, character;
+	private Graphics second;
+	private URL base;
 
 	@Override
 	public void init() {
-		setSize(800, 600);
+		setSize(800, 480);
 		setBackground(Color.BLACK);
 		setFocusable(true);
 		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Q-bot Alpha");
+		try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		// Image Setups
+		character = getImage(base, "data/character.png");
 	}
 
 	@Override
@@ -40,6 +53,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	@Override
 	public void run() {
 		while (true) {
+			robot.update();
 			repaint();
 			try {
 				Thread.sleep(17);
@@ -48,21 +62,49 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 			}
 		}
 	}
-	
-	
+
+	@Override
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(character, robot.getCenterX() - 61,
+				robot.getCenterY() - 63, this);
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_W:
+			System.out.println("Move up");
 			break;
 		case KeyEvent.VK_S:
+			System.out.println("Move down");
 			break;
 		case KeyEvent.VK_A:
+			robot.moveLeft();
 			break;
 		case KeyEvent.VK_D:
+			robot.moveRight();
 			break;
 		case KeyEvent.VK_SPACE:
+			System.out.println("Jump");
+			break;
+		case KeyEvent.VK_ESCAPE:
+			System.exit(ABORT);
 			break;
 		}
 
@@ -72,17 +114,21 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	public void keyReleased(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_W:
+			System.out.println("Stop moving up");
 			break;
 		case KeyEvent.VK_S:
+			System.out.println("Stop moving down");
 			break;
 		case KeyEvent.VK_A:
+			robot.stop();
 			break;
 		case KeyEvent.VK_D:
+			robot.stop();
 			break;
 		case KeyEvent.VK_SPACE:
+			System.out.println("Stop Jumping");
 			break;
 		}
-
 	}
 
 	@Override
